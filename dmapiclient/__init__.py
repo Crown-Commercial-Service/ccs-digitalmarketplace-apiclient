4354ff875e2e24a5be5dbd5f88f720a091cb38b5
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2024-07-24 15:20:55 +0100

    Add with evaluations to the find_framework_suppliers method

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '25.14.1'
+__version__ = '25.14.2'
 
 from .errors import APIError, HTTPError, InvalidResponse  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
