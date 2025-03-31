00303b4a9091b71d5d36e857469285db5c88d2a7
Osanda Gunasena <osanda.gunasena@redrockconsulting.co.uk>
2025-03-31 16:36:42 +0100

    Added audit type required for NDMP-540

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '32.2.3'
+__version__ = '32.2.4'
 
 from .errors import APIError, HTTPError, InvalidResponse, InvalidResponseType  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
