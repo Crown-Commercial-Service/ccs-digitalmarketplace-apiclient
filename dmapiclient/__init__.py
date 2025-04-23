e31ec6eb1eb277d8d507466d47a1fd2c8399adbb
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2025-04-23 12:41:06 +0100

    Pass the organisation name when sending the Spotlight warmup request

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '33.0.0'
+__version__ = '33.1.0'
 
 from .errors import APIError, HTTPError, InvalidResponse, InvalidResponseType  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
