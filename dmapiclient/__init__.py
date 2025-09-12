740646922bc3786be1ba96b0594abd337d775ee5
tim-s-ccs <timothy.south@crowncommercial.gov.uk>
2025-09-12 09:29:46 +0100

    Actually bump the version

--- dmapiclient/__init__.py
+++ dmapiclient/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '37.0.0'
+__version__ = '37.0.1'
 
 from .errors import APIError, HTTPError, InvalidResponse, InvalidResponseType  # noqa
 from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
